2c5e724efb0ea4962a3f7245425e8dfb99f2ed54
BlueBloodJM <jmrt.231298@gmail.com>
2017-07-11 10:24:58 -0500

    Add files via upload

---
+++ FrmIniciarSesion.java
@@ -0,0 +1,350 @@
+
+package Formularios;
+
+import Clases.Conectar;
+import static Formularios.FrmModificarUsuario.lblCargarFoto;
+import static Formularios.FrmModificarUsuario.txtCodigo;
+import com.sun.awt.AWTUtilities;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Shape;
+import java.awt.Toolkit;
+import java.awt.geom.RoundRectangle2D;
+import java.sql.*;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class FrmIniciarSesion extends javax.swing.JFrame {
+
+    Conectar cc = new Conectar();
+    Connection cn = cc.getConexion();
+    int x,y;
+
+    public FrmIniciarSesion() {
+        initComponents();
+        setLocationRelativeTo(null);
+        Shape forma= new RoundRectangle2D.Double(0,0,this.getBounds().width,this.getBounds().height,27,27);
+        AWTUtilities.setWindowShape(this, forma);
+        ((JPanel) getContentPane()).setOpaque(false);
+        ImageIcon uno = new ImageIcon(this.getClass().getResource("/Imagenes/fondoLogin.png"));
+        JLabel fondo = new JLabel();
+        fondo.setIcon(uno);
+        getLayeredPane().add(fondo, JLayeredPane.FRAME_CONTENT_LAYER);
+        //fondo.setBounds(0,0,uno.getIconWidth(),uno.getIconHeight());
+        fondo.setBounds(0, 0, 708, 340);
+        fondo.setBorder(BorderFactory.createMatteBorder(3,3,3,3,Color.BLACK));
+        txtUsuarios.requestFocus();
+        lblRuta.setVisible(false);
+    }
+
+    @Override
+    public Image getIconImage() {
+        Image retValue = Toolkit.getDefaultToolkit().
+                getImage(ClassLoader.getSystemResource("Imagenes/Escudo.png"));
+        return retValue;
+    }
+    
+    public void mostrarFoto(String usu){    
+        Conectar conec = new Conectar();
+        String sql="select usu_Usuario,usu_Foto from usuarios where usu_Usuario = '"+usu+"';";
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+            try {
+            ps = conec.getConexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                txtUsuarios.setText(rs.getString(1));
+                lblRuta.setText("D:\\Fotos_Usuario\\"+rs.getString(1)+".jpg");
+                Image i=null;
+                Blob blob= rs.getBlob(2);
+                i=javax.imageio.ImageIO.read(blob.getBinaryStream());
+                ImageIcon image =new ImageIcon(i.getScaledInstance(lblFoto.getWidth(), lblFoto.getHeight(), Image.SCALE_DEFAULT));
+                lblFoto.setIcon(image);
+            } 
+        }  
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                ps.close();
+                rs.close();
+                conec.desconectar();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    void acceder(String usuario, String pasword) {
+        String cap = "";
+        String sql = "select * from usuarios where usu_Usuario='" + usuario.toUpperCase() + "'and usu_Password='" + pasword.toUpperCase() + "';";
+        try {
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                cap = rs.getString("usu_Tipo");
+            }
+            if (cap.toUpperCase().equals("ADMINISTRADOR")) {
+                this.setVisible(false);
+                JOptionPane.showMessageDialog(this, "Bienvenido Sr(a): " +cap.toUpperCase()+": "+ usuario.toUpperCase());
+                FrmMenuPrincipal mp = new FrmMenuPrincipal();
+                mp.setVisible(true);
+                mp.pack();
+
+                FrmMenuPrincipal.lblUsuario.setText(cap.toUpperCase()+" - "+usuario.toUpperCase());
+            }
+            if (cap.toUpperCase().equals("OPERARIO")) {
+                this.setVisible(false);
+                JOptionPane.showMessageDialog(this, "Bienvenido Sr(a): " +cap.toUpperCase()+": "+ usuario);
+                FrmMenuPrincipal mp = new FrmMenuPrincipal();
+                mp.setVisible(true);
+                mp.pack();
+                FrmMenuPrincipal.lblUsuario.setText(cap.toUpperCase()+" - "+usuario.toUpperCase());
+                FrmMenuPrincipal.btnRegistro.setVisible(false);
+                FrmMenuPrincipal.btnGestionarAlumno.setVisible(false);
+                FrmMenuPrincipal.btnGestionarUsuario.setVisible(false);
+                FrmMenuPrincipal.btnReportes.setVisible(false);
+            }
+            if ((!cap.toUpperCase().equals("ADMINISTRADOR"))&& (!cap.toUpperCase().equals("OPERARIO"))) {
+                JOptionPane.showMessageDialog(this, "No Existe Los Datos");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FrmIniciarSesion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        txtUsuarios = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtPasword = new javax.swing.JPasswordField();
+        btnAceptar = new javax.swing.JButton();
+        btnCancelar = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel4 = new javax.swing.JLabel();
+        lblFoto = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        lblRuta = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("ACCESO AL SISTEMA");
+        setIconImage(getIconImage());
+        setMinimumSize(new java.awt.Dimension(670, 341));
+        setUndecorated(true);
+        setSize(new java.awt.Dimension(690, 386));
+        addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                formMouseDragged(evt);
+            }
+        });
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                formMouseClicked(evt);
+            }
+        });
+
+        txtUsuarios.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        txtUsuarios.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtUsuariosKeyReleased(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setText("USUARIO");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel2.setText("CONTRASEÑA");
+
+        txtPasword.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        btnAceptar.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
+        btnAceptar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Aseptarr.png"))); // NOI18N
+        btnAceptar.setText("INGRESAR");
+        btnAceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAceptarActionPerformed(evt);
+            }
+        });
+
+        btnCancelar.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
+        btnCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Cerrar.png"))); // NOI18N
+        btnCancelar.setText("SALIR");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
+
+        lblFoto.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblFoto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/login.png"))); // NOI18N
+        lblFoto.setToolTipText("");
+        lblFoto.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        lblFoto.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        lblFoto.setMaximumSize(new java.awt.Dimension(50, 50));
+        lblFoto.setMinimumSize(new java.awt.Dimension(50, 50));
+        lblFoto.setPreferredSize(new java.awt.Dimension(50, 50));
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/icono.png"))); // NOI18N
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 30)); // NOI18N
+        jLabel5.setText("INICIAR SESIÓN");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jLabel2)
+                                    .addComponent(txtUsuarios, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+                                    .addComponent(txtPasword))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
+                                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(39, 39, 39)
+                                .addComponent(lblFoto, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(41, 41, 41))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(81, 81, 81)
+                        .addComponent(btnAceptar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(152, 152, 152))))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel6)
+                .addGap(171, 171, 171)
+                .addComponent(jLabel5)
+                .addGap(39, 39, 39)
+                .addComponent(lblRuta, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(11, 11, 11)
+                        .addComponent(lblRuta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(8, 8, 8)
+                        .addComponent(txtUsuarios, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel2)
+                        .addGap(8, 8, 8)
+                        .addComponent(txtPasword, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(lblFoto, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 11, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(3, 3, 3)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnCancelar, javax.swing.GroupLayout.DEFAULT_SIZE, 52, Short.MAX_VALUE)
+                    .addComponent(btnAceptar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(15, 15, 15))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAceptarActionPerformed
+        String usu = txtUsuarios.getText();
+        String pass = new String(txtPasword.getPassword());
+        acceder(usu, pass);
+    }//GEN-LAST:event_btnAceptarActionPerformed
+
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnCancelarActionPerformed
+
+    private void formMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseDragged
+        Point p=MouseInfo.getPointerInfo().getLocation();
+        this.setLocation(p.x-x,p.y-y);
+    }//GEN-LAST:event_formMouseDragged
+
+    private void formMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseClicked
+        x=evt.getX();
+        y=evt.getY();
+    }//GEN-LAST:event_formMouseClicked
+
+    private void txtUsuariosKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUsuariosKeyReleased
+        mostrarFoto(txtUsuarios.getText());
+    }//GEN-LAST:event_txtUsuariosKeyReleased
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrmIniciarSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrmIniciarSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrmIniciarSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrmIniciarSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrmIniciarSesion().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAceptar;
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel lblFoto;
+    private javax.swing.JLabel lblRuta;
+    private javax.swing.JPasswordField txtPasword;
+    private javax.swing.JTextField txtUsuarios;
+    // End of variables declaration//GEN-END:variables
+}
